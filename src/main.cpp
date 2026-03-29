5da7948785a9e4f82aafdddb427d1f50728414ce
buteomont@gmail.com <buteomont@gmail.com>
2026-03-28 20:58:01 -0500

    Removed unnecessary uptime() code.

--- src/main.cpp
+++ src/main.cpp
@@ -408,20 +408,6 @@ void setup()
   rf.getModuleStatus();
   }
 
-// unsigned long uptime() {
-//   static unsigned long lastUptime = 0;
-//   static unsigned long uptimeAdd = 0;
-//   unsigned long uptime = millis() / 1000 + uptimeAdd;
-//   if (uptime < lastUptime) {
-//     uptime += 4294967;
-//     uptimeAdd += 4294967;
-//   }
-//   lastUptime = uptime;
-//   return uptime;
-// }
-
-// int next = uptime() + 30;
-
 #if defined(setBitrate) || defined(setFreqDev) || defined(setRxBW)
 
 #  ifdef setBitrate
